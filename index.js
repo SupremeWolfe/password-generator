9bb7e4ceba64684db2fa192c4f2e2263c2a78923
SupremeWolfe <johannes.laebe@gmx.de>
2023-08-09 20:48:03 +0200

    v2.1
    added input for password length
    max. 20 characters

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ const characLett = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 const characSpec = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+",
 "=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
+document.getElementById("lengthInput").value = 15
 
+let pw1 = ""
+let pw2 = ""
 
 
 
 let output1El = document.getElementById("output1-el")
 let output2El = document.getElementById("output2-el")
-let pwLength = 15
+let pwLength = document.getElementById("lengthInput").value
 
 let numbers = document.getElementById("numberToggle-el")
 let character = document.getElementById("characterToggle-el")
@@ -89,9 +92,13 @@ function genRanItem(){
 }
 
 function generatePw(){
-    let pw1 = ""
-    let pw2 = ""
-
+    
+    pwLength = document.getElementById("lengthInput").value
+    if (pwLength > 20){
+        pwLength = 20
+        document.getElementById("lengthInput").value = 20
+    }
+    
     for (i = 0; i < pwLength; i++){
         pw1 += genRanItem()
         pw2 += genRanItem()
@@ -99,7 +106,9 @@ function generatePw(){
 
     output1El.textContent = pw1
     output2El.textContent = pw2
+
 }
+
 
 /*function altGenPw(){
     let sum = []
